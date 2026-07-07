0e13c94849179773ae36aa0c8e4aca03cfc33c45
Mashrafee Aryan <121733421+MashrafeeAryan@users.noreply.github.com>
2026-07-07 08:45:21 -0500

    Time: 18 ms (62.57%) | Memory: 19.3 MB (52.86%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,36 +1,62 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
+        """
+        1. if s2> s1:
+        2. Sliding window. fixed
+        3. Define the first window which be s2[:len(s1)]
+        4. Avoid slicing strings
+        5. Keep a frequency count in dict where we can match the dict of the window and s1
+        6. To remove and add to sliding window:
+            1. we can reduce the count of the left most element in the window by 1
+            2. we increase the count for the new addition to sliding window
+
+        """
+
+        if len(s1) > len(s2):
             return False
+        
         k = len(s1)
+        
+        #S1 dict
         s1_freq = {}
+        s2_freq ={}
         for i in s1:
             if i in s1_freq:
                 s1_freq[i] +=1
             else:
                 s1_freq[i] = 1
-  
-        s2_freq = {}
-        for i in range(k):
-            if s2[i] in s2_freq:
-                s2_freq[s2[i]] +=1
-            else:
-                s2_freq[s2[i]] = 1
         
+        for i in s2[:k]:
+            if i in s2_freq:
+                s2_freq[i] +=1
+            else:
+                s2_freq[i] =1
+
         if s1_freq == s2_freq:
             return True
-
         for right in range(k, len(s2)):
             if s2[right] in s2_freq:
                 s2_freq[s2[right]] +=1
             else:
                 s2_freq[s2[right]] = 1
-            
-            if s2[right-k] in s2_freq:
-                s2_freq[s2[right - k]]-=1
 
-            if s2_freq[s2[right-k]] ==0:
+            s2_freq[s2[right-k]] -=1
+
+            if s2_freq[s2[right-k]] == 0:
                 del s2_freq[s2[right-k]]
-            if s2_freq == s1_freq:
+    
+            if s1_freq == s2_freq:
                 return True
         return False
+
+
+
+
+
+
+
+
+
+
+
+
