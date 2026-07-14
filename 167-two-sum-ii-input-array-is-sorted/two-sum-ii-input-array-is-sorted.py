b45289e85227a33dbba32c04cce9d854f242f507
Mashrafee Aryan <121733421+MashrafeeAryan@users.noreply.github.com>
2026-07-14 09:55:22 -0500

    Time: 3 ms (80.25%) | Memory: 20.4 MB (75.39%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         """
-        1. Have a pointer at left and a pointer at right
-        2. Add them up to see if they match target
-        3. If sum lower than target, move the left pointer it will increase the sum
-        4. If sum greater than target, move the right pointer it will decrease thes um
-        5. We are guranteed a solution if we use two pointers in O(n)
+        1. move from left and move righ
+        2. if total sum < target move left
+        3. if total sum> target move right
 
         """
         left = 0
@@ -13,11 +11,12 @@ class Solution:
 
         while left < right:
             current_sum = numbers[left] + numbers[right]
-
+            
             if current_sum < target:
                 left+=1
             elif current_sum > target:
-                right -=1
+                right-=1
+            
             else:
                 return [left+1, right+1]
     
