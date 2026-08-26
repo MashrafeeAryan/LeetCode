dc31e92ae07582607affc4301ca1b158a0107600
Mashrafee Aryan <121733421+MashrafeeAryan@users.noreply.github.com>
2026-08-26 00:12:41 -0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (18.03%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        A brute force would be two nested loops that add every number once.
+        An O(n) solution would be:
+        1. We can have a dict of all the elements in nums
+        2. then we can figure out by doing need = target - current
+        3. if need is in target we return that index. so dict probably needs to store key, value.
+        4. If 
+        """
+
         seen = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+        
+        for index, number in enumerate(nums):
+            need = target - number
+
+            if need in seen:
+                return [seen[need], index]
+            
+            seen[number] = index
