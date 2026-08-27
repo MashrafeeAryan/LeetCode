bae53f5b402fa36f08384f56bdcf00159ad48a47
Mashrafee Aryan <121733421+MashrafeeAryan@users.noreply.github.com>
2026-08-27 08:35:23 -0500

    Time: 51 ms (51.11%) | Memory: 28.7 MB (41.69%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        best_buy = prices[0]
-        best_profit = 0
+        """
+            We need to keep track of the best_profit so far and lowest buy day
+            Brute force would be just use two loops
+            however, we can optimize it and use one loop
+            1. best_profit = 0
+            2. lowest_buy_day = price[0]
+            3. for current_day in prices:
+                4. profit = current day - lowest buy day.
+                5. we find if it is our best profit so far using max(best_profit, profit)
+                6. We then also check if we encounter lowest_buy day a nother one lower than this one
+        """
 
-        for current_price in prices:
-            if current_price < best_buy:
-                best_buy = current_price
-            else:
-                profit = current_price - best_buy
-                best_profit = max(best_profit, profit)
-        
+        best_profit = 0
+        lowest_buy = prices[0]
+        for current_day in prices:
+            profit = current_day - lowest_buy
+            best_profit = max(best_profit, profit)
+
+            lowest_buy = min(lowest_buy, current_day)
+
         return best_profit
