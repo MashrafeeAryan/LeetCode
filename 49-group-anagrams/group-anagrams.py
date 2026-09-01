58bb15e9625a8fb4dba59e7424ee6b7a274525dd
Mashrafee Aryan <121733421+MashrafeeAryan@users.noreply.github.com>
2026-09-01 01:49:51 -0500

    Time: 15 ms (41.44%) | Memory: 23.9 MB (26.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,21 +1,23 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         """
-        1. We can loop through the array
-        2. Use counter to create a frequency of characters in each word
-        3. We use that frequency and convert it into tupple to use as a key
-        4. An anagram should have the same frequency so we use that as a key and if anagram has same key we just append it to the dict
-        5. We return a list of values 
-        """
-        from collections import Counter, defaultdict
-        
-        group = defaultdict(list)
+        We will do a count that uses 
+        A list where we use ord to keep frequnecy of each letter of key
+
+        """ 
+
+        groups ={}
         for word in strs:
+            count = [0]*26
+            for c in word:
+                count[ord(c) - ord("a")] +=1
 
-            freq = Counter(word)
+            key = tuple(count)
 
-            key = tuple(sorted(freq.items()))
+            if key not in groups:
+                groups[key] = [word]
+            else:
+                groups[key].append(word)
 
-            group[key].append(word)
 
-        return list(group.values())
+        return list(groups.values())
