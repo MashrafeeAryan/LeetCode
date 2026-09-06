21959aa83fa29600ea34179ab33ede749a79c8a1
Mashrafee Aryan <121733421+MashrafeeAryan@users.noreply.github.com>
2026-09-06 00:29:41 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (91.15%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,24 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         """
-        1. A while loop, if it is an opening bracket push it.
-        2. 
+        Every closing brackets must match the most recent opening bracket
+        1. We can create a dictionary where we have all the closing brackets associated with opening brackets
+        2. We can do a siple O(n) serch in dictioanry to see if this closing bracket is in the dict,
+        3. we check is it the closing bracket of the most recent opening bracket
+        4. to keep track of most recent opening bracket we use a stack
 
         """
 
         stack = []
-        parentheses_dict = {
-            ")": "(",
-            "]": "[",
-            "}": "{",
-        }
-        for i in range(len(s)):
-            if s[i] not in parentheses_dict:
-                stack.append(s[i])
-            else:
-                if not stack or stack[-1] != parentheses_dict[s[i]]:
-                    return False
 
+        pairs = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+
+        for i in s:
+            if i in "({[":
+                stack.append(i)
+            
+            else:
+                if not stack:
+                    return False
+                
+                if stack[-1] != pairs[i]:
+                    return False
+                
                 stack.pop()
-        
-        return not stack
+
+        return len(stack) == 0
+                
+            
