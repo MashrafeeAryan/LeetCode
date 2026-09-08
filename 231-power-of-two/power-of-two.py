e5489363053f83ff424d5f085e271a569e4b42a4
Mashrafee Aryan <121733421+MashrafeeAryan@users.noreply.github.com>
2026-09-07 23:28:50 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (85.86%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,3 +1,14 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and (n & (n-1)) == 0
+        """
+            If it si a power of two, covert that number in binary it will only have 1 and all other values 0s
+
+            and You can ensure if it has only one 1 by using bitwsie operator &. 
+
+            So you can say n-1 = 0111 and n = 1000. so we can do n & (n-1)
+            return n & (n-1) // true if power of two false if otherwise. now hwo do we deal with negative integers
+        """
+        if n<=0:
+            return False
+        
+        return (n & n-1) == 0
